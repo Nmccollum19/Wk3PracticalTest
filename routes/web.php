7d922c6118563018468962b9701f06134f86394c
Natalie McCollum <McCollum-N3@ulster.ac.uk>
2026-03-03 12:42:42 +0000

    Added method lab week 4

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,28 @@
 
 use Illuminate\Support\Facades\Route;
 
+class Job {
+    public static function all(): array
+    {
+        return [
+            [
+                'id' => 1,
+                'title' => 'Manager',
+                'salary' => '$50,000'
+            ],
+            [
+                'id' => 2,
+                'title' => 'Engineer',
+                'salary' => '$40,000'
+            ],
+            [
+                'id' => 3,
+                'title' => 'Technician',
+                'salary' => '$32,000'
+            ]
+        ];
+    }
+}
 
 Route::get('/', function () {
     return view('home');
@@ -9,53 +31,18 @@ Route::get('/', function () {
 
 Route::get('/jobs', function () {
     return view('jobs', [
-        'jobs' => [
-            [
-                'id'=>1,
-                'title' => 'Manager',
-                'salary' => '$50000'
-            ],
-            [
-                'id'=>2,
-                'title' => 'Engineer',
-                'salary' => '$40000'
-            ],
-            [
-                'id'=>3,
-                'title' => 'Technician',
-                'salary' => '$32000'
-            ]
-        ]
+        'jobs' => Job::all()
     ]);
 });
 
 Route::get('/jobs/{id}', function ($id) {
 
-    $jobs = [
-        [
-            'id' => 1,
-            'title' => 'Manager',
-            'salary' => '$50,000'
-        ],
-        [
-            'id' => 2,
-            'title' => 'Engineer',
-            'salary' => '$40,000'
-        ],
-        [
-            'id' => 3,
-            'title' => 'Assistant',
-            'salary' => '$30,000'
-        ]
-    ];
-
-    $job = \Illuminate\Support\Arr::first($jobs, function ($job) use ($id) {
+    $job = \Illuminate\Support\Arr::first(Job::all(), function ($job) use ($id) {
         return $job['id'] == $id;
     });
 
     return view('job', ['job' => $job]);
 });
-
 
 Route::get('/contact', function () {
     return view('contact');
