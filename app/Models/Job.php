477bbba146c6eb8fdf94139029dc97da82be2e1c
Natalie McCollum <McCollum-N3@ulster.ac.uk>
2026-03-18 11:25:26 +0000

    Finish lab week 4

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -1,29 +1,8 @@
 <?php
 namespace App\Models;
+use \Illuminate\Database\Eloquent\Model;
 
-class Job {
-    public static function all(): array
-    {
-        return [
-            [
-                'id' => 1,
-                'title' => 'Manager',
-                'salary' => '$50,000'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Engineer',
-                'salary' => '$40,000'
-            ],
-            [
-                'id' => 3,
-                'title' => 'Technician',
-                'salary' => '$32,000'
-            ]
-        ];
-    }
-    public static function find(int $id):array
-    {
-        return \Illuminate\Support\Arr::first(static::all(),fn($job)=>$job['id']==$id);
-    }
+class Job extends Model {
+    protected $table='job_listings';
+    protected $fillable=['title','salary'];
 }
